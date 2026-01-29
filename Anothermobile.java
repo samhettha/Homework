a37d6f6b416ef5a2f180afa9a70b4f7a5aa072b4
samhettha <same5milie12@gmail.com>
2026-01-29 23:22:33 +0530

    29th hw

---
+++ Anothermobile.java
@@ -0,0 +1,19 @@
+class AnotherMobile
+{
+	public static void main(String[] args)
+	{
+		String mobileName = Galaxy S24 Ultra;
+	 String mobileBrand = Samsung;
+	 float screenSize = 5.8;
+     short batteryCapacity = 5000;
+	 double price = 74000.58;
+	 
+	 System.out.println(mobileName);
+	System.out.println(mobileBrand);
+	System.out.println(screenSize);
+	System.out.println(batteryCapacity);
+	System.out.println(price);
+	 
+	}
+	
+}
